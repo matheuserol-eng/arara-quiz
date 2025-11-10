02cce58f760d68a2c99b0e18a5687d41f0297a4b
Matheus <matheuserol@gmail.com>
2025-11-10 18:53:10 -0300

    Configurado rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/HomePage";
+
 const App = () => {
   return (
-    <main className="min-h-dvh grid place-items-center bg-slate-50">
-      <h1 className="text-3xl font-bold text-slate-800">
-        Hello Tailwind + React!
-      </h1>
-    </main>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes> 
   );
 }
 
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+  return (
+    <main className="min-h-dvh grid place-items-center bg-slate-50">
+      <h1 className="text-3xl font-bold text-slate-800">
+        Hello Tailwind + React!
+      </h1>
+    </main>
+  );
+}
