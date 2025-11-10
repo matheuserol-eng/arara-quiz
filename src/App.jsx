95eb419fd594ae867c1b04471c3a232901af575a
Matheus <matheuserol@gmail.com>
2025-11-10 18:50:26 -0300

    Iniciado projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="min-h-dvh grid place-items-center bg-slate-50">
+      <h1 className="text-3xl font-bold text-slate-800">
+        Hello Tailwind + React!
+      </h1>
+    </main>
+  );
+}
+
+export default App
